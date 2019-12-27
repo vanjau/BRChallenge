2afda531e19f9b58dd9f3d9deb4391489289358f
vanja.ruzic <javruzic@gmail.com>
2019-12-27 12:07:58 +0100

    initial commit

---
+++ BRChallenge/BRChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRChallenge
+//
+//  Created by Vanja Ruzic on 27/12/2019.
+//  Copyright © 2019 Vanja Ruzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
